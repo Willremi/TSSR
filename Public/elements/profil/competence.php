52e14fc91d9fbff3c27d2fecdc3485cb7e4c7679
Rémi Willebrouck <willremi@gmail.com>
2023-07-16 13:52:56 +0200

    ajout logiciels dans compétences

--- Public/elements/profil/competence.php
+++ Public/elements/profil/competence.php
@@ -6,16 +6,39 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="d-flex justify-content-around">
-                    <div>DNS</div>
-                    <div>DHCP</div>
-                    <div>VPN</div>
-                    <div>HTTP</div>
-                    <div>SMTP</div>
-                    <div>TCP/IP</div>
-                    <div>ARP</div>
-                    <div>ICMP</div>
+                    <div class="technos">DNS</div>
+                    <div class="technos">DHCP</div>
+                    <div class="technos">VPN</div>
+                    <div class="technos">HTTP</div>
+                    <div class="technos">SMTP</div>
+                    <div class="technos">TCP/IP</div>
+                    <div class="technos">ARP</div>
+                    <div class="technos">ICMP</div>
                 </div>
             </div>
         </div>
+        <div id="ligneCompetence" class="my-4 mx-auto"></div>
+        <h5 class="card-title">Logiciels</h5>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-around">
+                    <div>Packet Tracer</div>
+                    <div>GLPI</div>
+                    <div>OCS Inventory</div>
+                    <div>Microsoft 365 (Teams, Word, Excel, PPT)</div>
+                    <div>Teamwiever</div>
+                    <div>AnyDesk</div>
+                </div>
+            </div>
+        </div>    
+        <!-- <h5 class="card-title">Certifications</h5>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
+                <span id="cisco">CCNA Cisco "Introduction to networks"</span> en cours
+            </div>
+        </div>
+        <div id="ligneCompetence" class="my-4 mx-auto"></div> -->
     </div>
 </div>
